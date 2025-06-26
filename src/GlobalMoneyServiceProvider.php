87d572298b31ff4ab8506e45ad85020d745b8a82
bagongd3 <bsgongde234@gmail.com>
2025-06-27 00:42:13 +0700

    Fix compsoser

--- src/GlobalMoneyServiceProvider.php
+++ src/GlobalMoneyServiceProvider.php
@@ -6,14 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class GlobalMoneyServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        // Merge default config jika belum dipublish
+        $this->mergeConfigFrom(__DIR__ . '/../config/global-money.php', 'global-money');
+    }
+    
     public function boot(): void
     {
+        // Publikasi file konfigurasi
         $this->publishes([
             __DIR__ . '/../config/global-money.php' => config_path('global-money.php'),
         ], 'global-money-config');
-
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/global-money.php', 'global-money'
-        );
     }
 }
