a2ae6c00d8c0cf10dad0d84bfd481933d8329304
bagongd3 <bsgongde234@gmail.com>
2025-06-28 06:43:25 +0700

    Manage Konflik

--- src/GlobalMoneyServiceProvider.php
+++ src/GlobalMoneyServiceProvider.php
@@ -8,13 +8,11 @@ class GlobalMoneyServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        // Merge default config jika belum dipublish
         $this->mergeConfigFrom(__DIR__ . '/../config/global-money.php', 'global-money');
     }
-    
+
     public function boot(): void
     {
-        // Publikasi file konfigurasi
         $this->publishes([
             __DIR__ . '/../config/global-money.php' => config_path('global-money.php'),
         ], 'global-money-config');
